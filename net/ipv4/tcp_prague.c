0baea9be67b425564d1dcc6d9c9267578a81a3ce
Chia-Yu <chia-yu.chang@nokia-bell-labs.com>
2023-08-10 21:48:27 +0200

    Revert part of pacing... no const

--- net/ipv4/tcp_prague.c
+++ net/ipv4/tcp_prague.c
@@ -283,10 +283,10 @@ static u64 prague_pacing_rate_to_bytes_in_flight(struct sock *sk)
 	u64 bytes_in_flight;
 
 	rtt = US2RTT((u64)tcp_sk(sk)->srtt_us);
-	//bytes_in_flight = (ca->rate_bytes * rtt + (1<<22)) >> 23;
-	//return bytes_in_flight;
-	bytes_in_flight = ((u128)ca->rate_bytes * (u128)rtt + (1<<22)) >> 23;
-	return (bytes_in_flight > UINT64_MAX) ? UINT64_MAX : (u64)bytes_in_flight;
+	bytes_in_flight = (ca->rate_bytes * rtt + (1<<22)) >> 23;
+	return bytes_in_flight;
+	//bytes_in_flight = ((u128)ca->rate_bytes * (u128)rtt + (1<<22)) >> 23;
+	//return (bytes_in_flight > UINT64_MAX) ? UINT64_MAX : (u64)bytes_in_flight;
 }
 
 /* RTT independence will scale the classical 1/W per ACK increase. */
@@ -322,7 +322,7 @@ exit:
 static void prague_update_pacing_rate(struct sock *sk)
 {
 	struct prague *ca = prague_ca(sk);
-	const struct tcp_sock *tp = tcp_sk(sk);
+	struct tcp_sock *tp = tcp_sk(sk);
 	u64 max_inflight;
 	u64 rate, burst;
 	u64 mtu;
@@ -338,14 +338,14 @@ static void prague_update_pacing_rate(struct sock *sk)
 		//tp->mss_cache = tcp_mtu_to_mss(sk, mtu);
 		u64 new_cwnd = div_u64(max_inflight + mtu - 1, mtu);
 		ca->frac_cwnd = new_cwnd << CWND_UNIT;
-		//if (tp->snd_cwnd > new_cwnd && tp->snd_cwnd > MIN_CWND) {
-		//	--tp->snd_cwnd;
-		//	tp->snd_ssthresh = tp->snd_cwnd;
-		//	prague_cwnd_changed(sk);
-		//} else if (tp->snd_cwnd < new_cwnd && tp->snd_cwnd < tp->snd_cwnd_clamp) {
-		//	++tp->snd_cwnd;
-		//	prague_cwnd_changed(sk);
-		//}
+		if (tp->snd_cwnd > new_cwnd && tp->snd_cwnd > MIN_CWND) {
+			--tp->snd_cwnd;
+			tp->snd_ssthresh = tp->snd_cwnd;
+			prague_cwnd_changed(sk);
+		} else if (tp->snd_cwnd < new_cwnd && tp->snd_cwnd < tp->snd_cwnd_clamp) {
+			++tp->snd_cwnd;
+			prague_cwnd_changed(sk);
+		}
 		rate = ca->rate_bytes;
 	} else {
 		//mtu = tcp_mss_to_mtu(sk, max_t(u32, ca->sys_mss, tp->mss_cache));
@@ -494,9 +494,9 @@ static void prague_update_alpha(struct sock *sk)
 
 	increase = (div_u64((PRAGUE_MAX_ALPHA - ecn_segs)*tcp_mss_to_mtu(sk, tp->mss_cache), prague_virtual_rtt(sk)) + 1) >> 1;
 	if (ecn_segs) {
-		u128 decrease_u128 = ((u128)ca->rate_bytes*(u128)tp->alpha) >> (PRAGUE_ALPHA_BITS + 1);
-		decrease = (decrease_u128 > UINT64_MAX) ? UINT64_MAX : 0;
-		//decrease = (ca->rate_bytes*tp->alpha) >> (PRAGUE_ALPHA_BITS + 1);
+		//u128 decrease_u128 = ((u128)ca->rate_bytes*(u128)tp->alpha) >> (PRAGUE_ALPHA_BITS + 1);
+		//decrease = (decrease_u128 > UINT64_MAX) ? UINT64_MAX : 0;
+		decrease = (ca->rate_bytes*tp->alpha) >> (PRAGUE_ALPHA_BITS + 1);
 	}
 	ca->rate_bytes = max_t(u64, ca->rate_bytes + increase - decrease, MINIMUM_RATE);
 
